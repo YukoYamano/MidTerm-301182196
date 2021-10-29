558a8092b307e8611d27a6151fdd860cdd93d1eb
Yuko <yyamano@my.centennialcollege.ca>
2021-10-29 19:42:53 -0400

    Connected to MongoDB atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book"
+  //"URI": "mongodb://localhost/book"
   //after setting up the mongoDB I will change here 
+  "URI":"mongodb+srv://yuko:Hr1UTjxG1r2IZblZ@mongodb.ivz6j.mongodb.net/book?retryWrites=true&w=majority"
   //TODO
 };

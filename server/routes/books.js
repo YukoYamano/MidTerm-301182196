1194bd590c2e8a54702b806012bdc876c4fbe03c
Yuko <yyamano@my.centennialcollege.ca>
2021-10-29 18:58:08 -0400

    Fixed bugs

--- server/routes/books.js
+++ server/routes/books.js
@@ -9,7 +9,7 @@ let Book = require('../models/books');
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  Book.find( (err, books) => {
+  Book.find((err, books) => {
     if (err) {
       return console.error(err);
     }
@@ -60,7 +60,7 @@ router.post('/details', (req, res, next) => {
 // GET the Book Details page in order to edit an existing Book
 router.get('/edit/:id', (req, res, next) => {
   let id = req.params.id;
-  console.log(Book.findById(id));
+  //console.log(Book.findById(id));
 
   Book.findById(id, (err, bookToEdit) => {
     if (err) {
@@ -113,7 +113,7 @@ router.get('/delete/:id', (req, res, next) => {
           console.log(err);
           res.end(err);    
       } else {
-          // refresh the book list
+          // refresh the books
           res.redirect('/books');
       }
   });
